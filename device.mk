06a08fed32db9d584c2e10f0b7b220e8c2b566e6
Arian <arian.kulmer@web.de>
2020-09-05 14:18:03 +0200

    courbet: Set hardware revision property
    The bootloader sets androidboot.hwversion, use this and set the ro.boot.hardware.revision property in order to show it in settings.
    
    Change-Id: I3c6c678bf055091ea9fcb2a1524d321e2c1be043

--- device.mk
+++ device.mk
@@ -35,6 +35,7 @@ PRODUCT_PACKAGES += \
 
 # Init scripts
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/etc/init.courbet.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.courbet.rc \
     $(LOCAL_PATH)/rootdir/etc/init.nfc.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.nfc.rc
 
 # IR
